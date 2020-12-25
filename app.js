efa7161f9949ab7aa48c32e0a1f6f8be03973c10
Jeremy Rice <jdavidrice@gmail.com>
2020-12-24 19:59:23 -0500

    add first variables for calculator function

---
+++ app.js
@@ -0,0 +1,15 @@
+// let waterAmount = $('#waterAmount').val().trim();
+// let ratio = $('#ratio').val().trim();
+// let coffeeAmount = $('#coffeeAmount').val().trim();
+
+$('#calculate').click(function () {
+  let waterAmount = $('#waterAmount').val().trim();
+  let ratio = $('#ratio').val().trim();
+  let coffeeAmount = $('#coffeeAmount').val().trim();
+
+console.log(waterAmount)
+console.log(ratio)
+console.log(coffeeAmount)
+
+});
+
